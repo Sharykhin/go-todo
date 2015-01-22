662276fd7abe374d9e7d4b801f57be9cf817adec
Sharykhin Siarhei <siarhei.sharykhin@gmail.com>
2015-01-22 19:49:57 +0300

    ISSUE-#3 has been completed

--- server.go
+++ server.go
@@ -3,8 +3,8 @@ package main
 import (
 	//"fmt"
 	"html/template"
-	"net/http"
 	"log"
+	"net/http"
 )
 
 type Todo struct {
@@ -18,24 +18,29 @@ type Data struct {
 
 var todos []Todo
 
-
-
 func defaultHandler(res http.ResponseWriter, req *http.Request) {
-	
+
 	t, err := template.ParseFiles("public/index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	t.Execute(res, struct {
 		Todos []Todo
 		Title string
-		}{Todos:todos})
+	}{Todos: todos})
+
+	/*tmpl := make(map[string]*template.Template)
+	tmpl["index.html"] = template.Must(template.ParseFiles("public/views/index.html", "public/views/base.html"))
+	tmpl["index.html"].ExecuteTemplate(res, "base", struct {
+		Todos []Todo
+		Title string
+	}{Todos: todos})*/
 }
 
 func editHandler(res http.ResponseWriter, req *http.Request) {
 
-	todoTitle := req.URL.Path[len("/edit/"):]	
+	todoTitle := req.URL.Path[len("/edit/"):]
 	t, err := template.ParseFiles("public/index.html")
 	if err != nil {
 		log.Fatal(err)
@@ -44,23 +49,38 @@ func editHandler(res http.ResponseWriter, req *http.Request) {
 	t.Execute(res, data)
 }
 
+func deletHandler(res http.ResponseWriter, req *http.Request) {
+	todoTitle := req.URL.Path[len("/delete/"):]
+	var tmpTodos []Todo
+	for _, value := range todos {
+		
+		if string(value.Title) != string(todoTitle) {
+
+			tmpTodos = append(tmpTodos, Todo{Title: string(value.Title)})
+
+		}
+	}
+	todos = tmpTodos
+	http.Redirect(res, req, "/", http.StatusFound)
+}
+
 func saveHandler(res http.ResponseWriter, req *http.Request) {
 
 	//Get todo
 	todoTitle := req.FormValue("todo")
 	prevTodo := req.FormValue("prev_todo")
-	
+
 	if len(prevTodo) > 0 {
 		var tmpTodos []Todo
 		var toAppend bool = true
 		for _, value := range todos {
-			
+
 			if string(value.Title) == string(prevTodo) {
-					tmpTodos = append(tmpTodos,Todo{Title:todoTitle})	
-					toAppend = false	
-				} else {
-					tmpTodos = append(tmpTodos,Todo{Title:string(value.Title)})		
-				}
+				tmpTodos = append(tmpTodos, Todo{Title: todoTitle})
+				toAppend = false
+			} else {
+				tmpTodos = append(tmpTodos, Todo{Title: string(value.Title)})
+			}
 		}
 		todos = tmpTodos
 		if toAppend == true {
@@ -69,19 +89,21 @@ func saveHandler(res http.ResponseWriter, req *http.Request) {
 		}
 	} else {
 		todos = append(todos, Todo{Title: todoTitle})
-	}	
-	
+	}
+
 	http.Redirect(res, req, "/", http.StatusFound)
 
 }
 
 func main() {
-	
-	http.Handle("/vendor/", http.StripPrefix("/vendor/",http.FileServer(http.Dir("public/vendor"))))
 
-	http.HandleFunc("/", defaultHandler)	
+	http.Handle("/vendor/", http.StripPrefix("/vendor/", http.FileServer(http.Dir("public/vendor"))))
+
+	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/save", saveHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/delete/", deletHandler)
 
 	http.ListenAndServe(":8080", nil)
+
 }
